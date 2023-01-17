130ff09102572b8985f6fb90c4fa0807ef9dfbd3
Kaganzi Timothy <kagsburg@gmail.com>
2023-01-17 08:11:28 +0300

    first commit

---
+++ database/migrations/2023_01_16_212654_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->bigInteger('column_id')->unsigned();
+            $table->foreign('column_id')->references('id')->on('columns');
+            $table->string('is_deleted')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};
